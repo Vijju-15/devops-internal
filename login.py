a54d10c84e240b92f817883794abe4b0225b60c3
Vijju-15 <vijju152005@gmail.com>
2025-09-24 10:21:04 +0530

    result code

--- login.py
+++
@@ -1,9 +0,0 @@
-def Login(name, password):
-    if name == "Pavan" and password == "secret":
-        print("Login successful")
-        print("Welcome, Pavan!")
-        return True
-    else:
-        print("Login failed")
-        return False
-
